1d61e40198925df050bdbf3c94c92747c21bea44
boyanaaaa <125132910+boyanaaaa@users.noreply.github.com>
2025-11-18 14:20:52 +0200

    The main project page has been created, which  contains a project container on the left and   a main container on the right, which contains   all the tasks from the project. A container with the task structure has also been created.

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -2,12 +2,43 @@ import { greeting } from "./greeting.js";
 import "./styles.css";
 import odinImage from "./odin.png";
 
-
-
 console.log(greeting);
-
-
 const image = document.createElement("img");
 image.src = odinImage;
-   
 document.body.appendChild(image);
+
+
+const todosContainer = document.querySelector(".showTodosContainer");
+
+
+
+
+const todo = document.createElement("div");
+todo.classList.add("todo");
+todosContainer.appendChild(todo);
+
+const todoTitle = document.createElement("h2");
+todoTitle.textContent = "Go shopping";
+todo.appendChild(todoTitle);
+
+const todoDesription = document.createElement("p");
+todoDesription.textContent = "Shopping List: ****";
+todo.appendChild(todoDesription);
+
+const todoDate = document.createElement("input");
+todoDate.setAttribute("id", "todoDate");
+todoDate.setAttribute("type", "date");
+todo.appendChild(todoDate);
+
+const todoPriority = document.createElement("select");
+todoPriority.id = "todoPriority";
+["Low", "Medium", "High"].forEach(level => {
+  const option = document.createElement("option");
+  option.value = level;
+  option.textContent = level;
+  todoPriority.appendChild(option);
+});
+todo.appendChild(todoPriority);
+
+
+
