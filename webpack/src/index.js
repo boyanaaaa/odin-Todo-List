715199b6aa968ab23ab8e3bddea3f33da84fa455
boyanaaaa <125132910+boyanaaaa@users.noreply.github.com>
2025-11-21 15:37:01 +0200

    The button for adding projects works. After clicking, a new window appears in which the name of the new project must be written.  There is a cancel button that closes the window  and a button for adding a project that closes  the window and creates the new project, which  is visualized in the left project container.

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -11,7 +11,7 @@ const cancelBtn = document.querySelector(".cancelBtn");
 const createProjectBtn = document.querySelector(".createProjectBtn");
 const overlayDiv = document.querySelector(".overlay");
 const formNewProject = document.querySelector(".formNewProject");
-
+const newProjectsContainer = document.querySelector(".newProjectsContainer");
 
 
 
@@ -29,6 +29,28 @@ cancelBtn.addEventListener("click", () => {
 })
 
 
+createProjectBtn.addEventListener("click", ()=> {
+    overlayDiv.classList.add("hidden");
+    formNewProject.classList.add("hidden"); 
+
+    const newProject = document.createElement("div");
+    newProject.classList.add("newProject");
+    newProject.innerHTML = `
+    <h2>${projectInput.value}</h2>
+    <button class="removeBtn">Remove</button>
+  `;
+
+    const removeBtn = newProject.querySelector(".removeBtn");
+      removeBtn.addEventListener("click", () => {
+          newProject.remove();  // ← изтрива точно този елемент
+      });
+
+
+
+    newProjectsContainer.appendChild(newProject);
+    projectInput.value = "";
+
+})
 
 
 
@@ -36,7 +58,8 @@ cancelBtn.addEventListener("click", () => {
 
 
 
-/* Single Todo */
+
+/* Single Todo  */
 const todo = document.createElement("div");
 todo.classList.add("todo");
 todosContainer.appendChild(todo);
@@ -65,4 +88,43 @@ todoPriority.id = "todoPriority";
 todo.appendChild(todoPriority);
 
 
+/*
+function createTodo({ title, description, date, priority }) {
+  const todo = document.createElement("div");
+  todo.classList.add("todo");
 
+  todo.innerHTML = `
+    <h2>${title}</h2>
+    <p>${description}</p>
+    <input type="date" value="${date}">
+  `;
+
+  const todoPriority = document.createElement("select");
+  ["Low", "Medium", "High"].forEach(level => {
+    const option = document.createElement("option");
+    option.value = level;
+    option.textContent = level;
+    if (level === priority) option.selected = true;
+    todoPriority.appendChild(option);
+  });
+
+  todo.appendChild(todoPriority);
+  return todo;
+}
+
+const todoData = {
+  title: titleInput.value,
+  description: descriptionInput.value,
+  date: dateInput.value,
+  priority: priorityInput.value
+}
+
+todosContainer.appendChild(createTodo(todoData));
+todosContainer.appendChild(createTodo({
+      title: "Shopping",
+      description: "My shopping list: ****",
+      date: "2021-03-22",
+      priority: "High"
+}));
+
+*/
